ab3a378c5afdf1408f24a2188986cc79998b4bac
手瓜一十雪 <nanaeonn@outlook.com>
2023-12-04 16:25:38 +0800

    Create CareerMeituan.py

---
+++ CareerMeituan.py
@@ -0,0 +1,18 @@
+import requests
+import json
+
+# pageSize默认为10 这里填 9999 以便单APi输出全部 categoryId为筛选类型
+MainUrl = "https://zhaopin.meituan.com/api/official/job/getJobList"
+postData = '{"page":{"pageNo":1,"pageSize":9999},"jobShareType":"1","keywords":""}'
+headers = {"content-type": "application/json"}
+# 直接json解析
+ResultJSON = json.loads(requests.post(MainUrl, data=postData,headers=headers).text)
+# 提出Posts
+print(ResultJSON)
+ResultPosts = ResultJSON["data"]["list"]
+for index in range(0, len(ResultPosts)):
+    RecruitPostName = ResultPosts[index]["name"]  # 职位
+    Responsibility = ResultPosts[index]["jobDuty"]  # 能力
+    highLight = ResultPosts[index]["highLight"]  # 工作经验需求
+    # department = ResultPosts[index]["department"][0]["name"]  # 有的没写
+    print(index, RecruitPostName, Responsibility, highLight, )
