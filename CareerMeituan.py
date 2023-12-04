a2b503f32f7c5fec846921c072a6c54963344e72
手瓜一十雪 <nanaeonn@outlook.com>
2023-12-04 16:27:09 +0800

    Update CareerMeituan.py

--- CareerMeituan.py
+++ CareerMeituan.py
@@ -1,14 +1,13 @@
 import requests
 import json
 
-# pageSize默认为10 这里填 9999 以便单APi输出全部 categoryId为筛选类型
+# pageSize默认为10 这里填 9999 
 MainUrl = "https://zhaopin.meituan.com/api/official/job/getJobList"
 postData = '{"page":{"pageNo":1,"pageSize":9999},"jobShareType":"1","keywords":""}'
 headers = {"content-type": "application/json"}
 # 直接json解析
 ResultJSON = json.loads(requests.post(MainUrl, data=postData,headers=headers).text)
 # 提出Posts
-print(ResultJSON)
 ResultPosts = ResultJSON["data"]["list"]
 for index in range(0, len(ResultPosts)):
     RecruitPostName = ResultPosts[index]["name"]  # 职位
